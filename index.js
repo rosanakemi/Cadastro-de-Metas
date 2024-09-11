cba4eff101737ee7c3afcf188281bced84cf5d72
Rosana <rosanaluzia7@gmail.com>
2024-09-11 13:51:32 -0300

    apresenta metas realizadas

--- index.js
+++ index.js
@@ -1,86 +1,108 @@
-const { select, input, checkbox} = require('@inquirer/prompts')
+const { select, input, checkbox } = require("@inquirer/prompts");
 
 let meta = {
-    value: "tomar 3l de agua por dia",
-    checked: false,
-}
+  value: "tomar 3l de agua por dia",
+  checked: false,
+};
 
-let metas = [ meta ]
+let metas = [meta];
 
 const cadastarMeta = async () => {
-    const meta = await input({ message: "Digite a meta: "})
+  const meta = await input({ message: "Digite a meta: " });
 
-    if(meta.length == 0) {
-        console.log("a meta não pode ser vazia")
-        return
-    }
+  if (meta.length == 0) {
+    console.log("a meta não pode ser vazia");
+    return;
+  }
 
-    metas.push(
-        { value: meta, checked: false },
-    )
-}
+  metas.push({ value: meta, checked: false });
+};
 
 const listarMetas = async () => {
-    const respostas = await checkbox({
-        message: "use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
-        choices: [...metas],
-        instructions: false,
+  const respostas = await checkbox({
+    message:
+      "use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+    choices: [...metas],
+    instructions: false,
+  });
+
+  if (respostas.length == 0) {
+    console.log("nenhuma meta selecionada!");
+    return;
+  }
+
+  metas.forEach((m) => {
+    m.checked = false;
+  });
+
+  respostas.forEach((resposta) => {
+    const meta = metas.find((m) => {
+      return m.value == resposta;
+    });
+
+    meta.checked = true;
+  });
+  console.log(" Meta concluída!");
+};
+
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) =>{
+        return meta.checked
     })
 
-    if(respostas.length == 0){
-        console.log("nenhuma meta selecionada!")
+    if (realizadas.length == 0) {
+        console.log ("Não existem metas realizadas")
         return
     }
 
-    metas.forEach((m) =>{
-        m.checked = false
+    await select({
+        message: "metas realizadas",
+        choices: [...realizadas]
     })
-
-    respostas.forEach((resposta) => {
-        const meta = metas.find((m)=>{
-            return m.value == resposta
-        })
-
-        meta.checked = true
-    })
-    console.log(" Meta concluída!")
 }
 
-const start = async() => {
+const start = async () => {
+  while (true) {
+    const opcao = await select({
+      message: "Menu >",
+      choices: [
+        {
+          name: "cadastrar meta",
+          value: "cadastrar",
+        },
+        {
+          name: "listar metas",
+          value: "listar",
+        },
+        {
+          name: "metas realizadas",
+          value: "realizadas",
+        },
+        {
+          name: "sair",
+          value: "sair",
+        },
+      ],
+    });
 
-    while (true){
-        
-        const opcao = await select({
-            message: "Menu >",
-            choices: [
-                {
-                    name: "cadastrar meta",
-                    value: "cadastrar"
-                },
-                {
-                    name: "listar metas",
-                    value: "listar"
-                },
-                {
-                  name: "sair",
-                  value: "sair"  
-                }
-            ]
-        })
+    switch (opcao) {
+      case "cadastrar":
+        await cadastarMeta();
+        console.log(metas);
+        break;
 
-        switch(opcao){
-            case "cadastrar":
-             await cadastarMeta()
-             console.log(metas)
-            break
-            case "listar":
-            await listarMetas()
-            break
-            case "sair":
-                console.log("ATE A PROXIMA!")
-            return
-        }
+      case "listar":
+        await listarMetas();
+        break;
+
+      case "realizadas":
+        await metasRealizadas();
+        break;
+
+      case "sair":
+        console.log("ATE A PROXIMA!");
+        return;
     }
-    
-}
-start()
+  }
+};
+start();
